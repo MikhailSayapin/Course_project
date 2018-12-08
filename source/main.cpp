520902dec92263e8bb4491b74ee41a50c7f3a536
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-12-09 00:26:33 +0300

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -1,25 +1,42 @@
 #include "map.h"
+#include "traffic_lights.h"
 #include "window.h"
+#include "player.h"
 #include <SFML/Graphics.hpp>
 
-using namespace sf; 
+using namespace sf;
 
 int main()
 {
 	setlocale(LC_ALL, "ru");
-	RenderWindow window(VideoMode(1346, 748), "Misha");
-	create_map_sprite();
+	RenderWindow window(VideoMode(1680, 1050), "Misha");
+	view.reset(sf::FloatRect(0, 0, 1680, 1050));
+	Map::create_map_sprite();
+	Lights::create_map_sprite();
+	Clock cl;
+	float current_frame = 0;
+	Player our_car(0, 0, 115, 47);
 	while (window.isOpen())
 	{
-		window.clear();
+		float time = cl.getElapsedTime().asMicroseconds();
+		cl.restart();
+		time /= 800;
+		window.setView(view);
+		view_map(time);
 		Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		draw_map(window);
-		window.draw(sp_map);
+		our_car_move(our_car, time);
+		Map::draw_map(window);
+		Lights::draw_map(window);
+		window.draw(Map::sp_map);
+		window.draw(Lights::sp_map);
+		window.draw(our_car.get_sprite());
 		window.display();
+		window.clear();
+		Lights::switching();
 	}
 }
