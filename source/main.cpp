751cb2d8448b33cae69791b407f983d42a05fa50
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-12-09 02:42:04 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -12,7 +12,6 @@ int main()
 	RenderWindow window(VideoMode(1680, 1050), "Misha");
 	view.reset(sf::FloatRect(0, 0, 1680, 1050));
 	Map::create_map_sprite();
-	Lights::create_map_sprite();
 	Clock cl;
 	float current_frame = 0;
 	Player our_car(0, 0, 115, 47);
@@ -31,12 +30,10 @@ int main()
 		}
 		our_car_move(our_car, time);
 		Map::draw_map(window);
-		Lights::draw_map(window);
 		window.draw(Map::sp_map);
-		window.draw(Lights::sp_map);
 		window.draw(our_car.get_sprite());
+		Lights::draw_traffic_lights(window);
 		window.display();
 		window.clear();
-		Lights::switching();
 	}
 }
