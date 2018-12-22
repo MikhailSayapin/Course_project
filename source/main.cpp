cd81d97b1ac8d410d3b9447e155e58528c76082b
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2018-12-23 00:36:32 +0300

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,7 @@
 #include "window.h"
 #include "player.h"
 #include "car.h"
+#include "speed_regulation.h"
 #include <SFML/Graphics.hpp>
 
 using namespace sf;
@@ -15,7 +16,7 @@ int main()
 	Map::create_map_sprite();
 	Clock cl;
 	float current_frame = 0;
-	Player our_car(1960, 1130, 70, 28);
+
 	while (window.isOpen())
 	{
 		float time = cl.getElapsedTime().asMicroseconds();
@@ -29,13 +30,17 @@ int main()
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		our_car_move(our_car, time);
+
+		Player::our_car_move(Player::our_car, time);
 		Other_Car::run(time);
 		Map::draw_map(window);
+		Signs::draw(window);
 		window.draw(Map::sp_map);
-		window.draw(our_car.get_sprite());
+		window.draw(Player::our_car.get_sprite());
 		Lights::draw_traffic_lights(window);
 		Other_Car::draw(window);
+		Player::crash_run(window);
+		Player::message(window);
 		window.display();
 		window.clear();
 	}
